74a7a6705c801016c8157b185c07323c16e78ea6
cquaggio <christian.quaggio@alkemy.com>
2023-03-09 00:16:43 +0100

    update scripts

--- load_psql.sh
+++ load_psql.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Connessione al database PostgreSQL
-PGHOST="db"
-PGPORT="5432"
+PGHOST="localhost"
 PGUSER="dlbd"
 PGPASSWORD="admin2022"
 PGDATABASE="master_db"
@@ -12,29 +11,18 @@ export PGPASSWORD
 TABLE_NAME="earthquake_data"
 
 # File CSV di input
-INPUT_CSV="earthquake.csv"
+INPUT_CSV="earthquakes.csv"
 
 # Creazione della tabella
-psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -c "
+psql -h $PGHOST -U $PGUSER -d $PGDATABASE -c "
   CREATE TABLE IF NOT EXISTS public.$TABLE_NAME (
-    time timestamp with time zone,
-    latitude double precision,
-    longitude double precision,
-    depth double precision,
-    magnitude double precision,
-    magtype text,
-    mb double precision,
-    ms double precision,
-    mww double precision,
-    place text
+    Time date,
+    Latitude double precision,
+    Longitude double precision,
+    Place text,
+    Mag double precision
   );
 "
 
 # Inserimento dei dati dalla tabella CSV
-psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -c "
-  COPY public.$TABLE_NAME(time, latitude, longitude, depth, magnitude, magtype, mb, ms, mww, place)
-  FROM '$INPUT_CSV'
-  DELIMITER ','
-  CSV HEADER;
-"
-
+psql -h $PGHOST -U $PGUSER -d $PGDATABASE -c "\copy earthquake_data(Time, Latitude, Longitude, Place, Mag) FROM 'earthquakes.csv' WITH (FORMAT csv, HEADER true);"
